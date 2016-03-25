aada9acdcb37cc2e800a440cb4c2a3f8b5e3db9e
Prelang Builder <builder@prelang.com>
2016-03-25 03:06:05 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'School' to allow for create, update

--- app/admin/school.rb
+++ app/admin/school.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register School do
+  permit_params :school_name, :description
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
